58d9c51d92b6a46b28ffa4a288c4d508f8246c04
Jacob Seppanen <jbseppanen@gmail.com>
2019-04-24 12:06:15 -0700

    update player class function to pick up an item.

--- src/item.py
+++ src/item.py
@@ -1,6 +1,5 @@
 # Class to hold item information
 
-
 class Item:
     def __init__(self, name):
         self.name = name
--- src/player.py
+++ src/player.py
@@ -1,17 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from .room import Room
 
 
 class Player:
     def __init__(self, name, room):
         self.name = name
         self.room = room
-        self.items = []
+        self.inventory = []
 
     def pickup(self, item):
-        self.items.append(item)
-
-
-class Item:
-    def __init__(self, name):
-        self.name = name
+        if self.room.items.count(item) > 0:
+            self.inventory.append(item)
+            self.room.items.remove(item)
+            print(f"The {item.name} has been picked up.")
+        else:
+            print(f"A {item.name} is not in this room.")
