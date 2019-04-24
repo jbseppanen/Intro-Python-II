13a6d052b15540455d6026682481680fbe3df322
Jacob Seppanen <jbseppanen@gmail.com>
2019-04-24 11:54:54 -0700

    Add function to Room class to print out names of items in itself.

--- src/room.py
+++ src/room.py
@@ -11,3 +11,8 @@ class Room:
         self.s_to = None
         self.e_to = None
         self.w_to = None
+
+    def print_items(self):
+        print("This room has the following items: ")
+        for i in self.items:
+            print(i.name)
